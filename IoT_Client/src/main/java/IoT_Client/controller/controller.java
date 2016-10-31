da8dd33407976c79e4e0948c82c6abd9bcad4e25
Hamza Ghani <h.u.f.ghani@gmail.com>
2016-10-31 16:04:14 +0000

    RFID and class that I will need

--- IoT_Client/src/main/java/IoT_Client/controller/controller.java
+++ IoT_Client/src/main/java/IoT_Client/controller/controller.java
@@ -3,9 +3,14 @@ package IoT_Client.controller;
 /**
  * Created by hamzaghani on 28/10/2016.
  */
-public class controller {
+public class Controller {
+
+
+
 
     public static void main(String[] args){
 
     }
+
+
 }
---
+++ IoT_Client/src/main/java/IoT_Client/sensors/Data2Server.java
@@ -0,0 +1,8 @@
+package IoT_Client.sensors;
+
+/**
+ * Created by hamzaghani on 31/10/2016.
+ */
+public class Data2Server {
+
+}
