fdde9423596de238937720c5c97f7411b96f89b1
Hamza Ghani <h.u.f.ghani@gmail.com>
2016-11-03 21:31:01 +0000

    MainActivity

--- IoT_Mobile/app/src/main/java/com/iot_mobile/hamzaghani/iot_mobile/MainActivity.java
+++ IoT_Mobile/app/src/main/java/com/iot_mobile/hamzaghani/iot_mobile/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    `ŸŸŸŸ`
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
