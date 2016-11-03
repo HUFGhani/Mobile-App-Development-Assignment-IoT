2e6d52f06eba0836bb1867a39f2b86aab53e0460
Hamza Ghani <h.u.f.ghani@gmail.com>
2016-11-03 20:48:54 +0000

    starting point for the mobile application

---
+++ IoT_Mobile/app/src/main/java/com/iot_mobile/hamzaghani/iot_mobile/MainActivity.java
@@ -0,0 +1,16 @@
+package com.iot_mobile.hamzaghani.iot_mobile;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    `ŸŸŸŸ`
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
